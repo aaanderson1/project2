e7262ccbfbeb80f7f29fbf20d33416b67b299f7f
Francisco Cagnoni <fcagnoni@Franciscos-MacBook-Pro.local>
2019-06-27 21:03:11 -0600

    added some routes that maybe work

--- models/reader.js
+++ models/reader.js
@@ -2,7 +2,6 @@ module.exports = function(sequelize, DataTypes) {
   var Reader = sequelize.define("Reader", {
     name: DataTypes.STRING
   });
-
   Reader.associate = function(models) {
     Reader.hasMany(models.Book, {
       onDelete: "cascade"
