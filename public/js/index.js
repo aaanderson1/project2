92a85b1167b6f8dc8cb4f7ab8a755c304e6b9f22
Allison <andersonaallison@gmail.com>
2019-07-06 14:27:32 -0600

    index changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,80 +1,108 @@
-$(document).ready(function() {
-  $(".parallax").parallax();
-// Get references to page elements
-// eslint-disable-next-line no-unused-vars
+$(document).ready(function () {
+    $(".parallax").parallax();
+    // Get references to page elements
+    // eslint-disable-next-line no-unused-vars
 
 
+    var userName = $("#userName");
+    var bookTitle = $("#bookTitle");
+    var authorFirstName = $("#authorFirst");
+    var authorLastName = $("#authorLast");
+    var genre = $("#genre");
+    var pagesNumber = $("#pagesNumber");
+    var comments = $("#book-description");
+    var rating = $("#rating");
+    var imageURL = $("#imageURL");
+    // eslint-disable-next-line no-unused-vars
+    var submitBtnCurrent = $("#submitCurrent");
+    // eslint-disable-next-line no-unused-vars
+    var submitBtnPast = $("#submitAlready");
+    // eslint-disable-next-line no-unused-vars
+    var submitBtnWishlist = $("#submitWishlist");
+    var bookList = $("#book-list");
 
-// eslint-disable-next-line no-unused-vars
-var submitBtnCurrent = $("#submitCurrent");
-// eslint-disable-next-line no-unused-vars
-var submitBtnPast = $("#submitAlready");
-// eslint-disable-next-line no-unused-vars
-var submitBtnWishlist = $("#submitWishlist");
-var bookList = $("#book-list");
+    var bookService = new BookService();
 
-var bookService = new BookService();
+    // refreshbooks gets new books from the db and repopulates the list
+    var refreshbooks = function () {
+        bookService.getBooks().then(function (data) {
+            var $books = data.map(function (book) {
+                var $a = $("<a>")
+                    .text(book.text)
+                    .attr("href", "/book/" + book.id);
 
-// refreshbooks gets new books from the db and repopulates the list
-var refreshbooks = function() {
-    bookService.getbooks().then(function(data) {
-    var $books = data.map(function(book) {
-      var $a = $("<a>")
-        .text(book.text)
-        .attr("href", "/book/" + book.id);
+                var $li = $("<li>")
+                    .attr({
+                        class: "list-group-item",
+                        "data-id": book.id
+                    })
+                    .append($a);
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": book.id
-        })
-        .append($a);
+                var $button = $("<button>")
+                    .addClass("btn-large waves-effect waves-light blue-grey darken-1 fas fa-plus")
+                    .text("ｘ");
 
-      var $button = $("<button>")
-        .addClass("btn-large waves-effect waves-light blue-grey darken-1 fas fa-plus")
-        .text("ｘ");
+                $li.append($button);
 
-      $li.append($button);
+                return $li;
+            });
 
-      return $li;
-    });
+            bookList.empty();
+            bookList.append($books);
+        });
+    };
 
-    bookList.empty();
-    bookList.append($books);
-  });
-};
+    // handleFormSubmit is called whenever we submit a new book
+    // Save the new book to the db and refresh the list
+    var handleFormSubmit = function () {
 
-// handleFormSubmit is called whenever we submit a new book
-// Save the new book to the db and refresh the list
-var handleFormSubmit = function() {
-  
+        var book = {
+            name: userName.val(),
+            title: bookTitle.val(),
+            authorFirst: authorFirstName.val(),
+            authorLast: authorLastName.val(),
+            genre: genre.val(),
+            pages: pagesNumber.val(),
+            comments: comments.val(),
+            rating: rating.val(),
+            image: imageURL.val()
+        };
 
+        if (!(book.name && book.title && book.authorFirst && book.authorLast && book.genre && book.pages && book.comments && book.rating &&
+                book.image)) {
+            alert("You must enter your name, book title, author first name, author last name, genre, numbe of pages, comments, rating and image URL!");
+            return;
+        }
 
-  API.saveBook(book).then(function() {
-    refreshbooks();
-  });
+        bookService.createBook(book).then(function () {
+            userName.val("");
+            bookTitle.val("");
+            authorFirstName.val("");
+            authorLastName.val("");
+            genre.val("");
+            pagesNumber.val("");
+            comments.val("");
+            rating.val("");
+            imageURL.val("");
+            refreshbooks();
+        });
+    };
 
-  bookService.createBook(book).then(function() {
-    refreshbooks();
-  });
+    // handleDeleteBtnClick is called when an book's delete button is clicked
+    // Remove the book from the db and refresh the list
+    var handleDeleteBtnClick = function () {
+        var idToDelete = $(this)
+            .parent()
+            .attr("data-id");
 
+        bookService.deleteBook(idToDelete).then(function () {
+            refreshbooks();
+        }).catch(function (err){
+            console.log(err);
+        });
+    };
 
- 
-};
-
-// handleDeleteBtnClick is called when an book's delete button is clicked
-// Remove the book from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  bookService.deleteBook(idToDelete).then(function() {
-    refreshbooks();
-  });
-};
-
-// Add event listeners to the submit and delete buttons
-submitBtnCurrent.on("click", handleFormSubmit);
-bookList.on("click", ".delete", handleDeleteBtnClick);
+    // Add event listeners to the submit and delete buttons
+    submitBtnCurrent.on("click", handleFormSubmit);
+    bookList.on("click", ".delete", handleDeleteBtnClick);
 });
