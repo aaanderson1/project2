fa03fa82d0f17ba38bd1a40f49c47eb7121b3556
Jenny Ivy <47339542+jenivy06@users.noreply.github.com>
2019-07-06 16:49:44 -0600

    updated jquery for wishlist and already read tables

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".parallax").parallax();
+ 
 // Get references to page elements
 // eslint-disable-next-line no-unused-vars
 
@@ -49,24 +49,12 @@ var refreshbooks = function() {
 var handleFormSubmit = function() {
   
 
-<<<<<<< HEAD
   //API.saveBook(book).then(function() {
     //refreshbooks();
   //});
   bookService.createBook(book).then(function() {
     refreshbooks();
   });
-=======
-
-  API.saveBook(book).then(function() {
-    refreshbooks();
-  });
-
-  bookService.createBook(book).then(function() {
-    refreshbooks();
-  });
-
->>>>>>> 29f84cd3a1a5daa2764f11ca6d598679dad5eddc
 
  
 };
